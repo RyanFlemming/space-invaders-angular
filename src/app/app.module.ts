b302b6d51f5d60200bea06e9993970cad801e737
Ryan Flemming <rmf919@live.com>
2023-02-22 13:24:32 +1100

    add initial classes for game objects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GameCanvasComponent } from './app/game-canvas/game-canvas.component';
+import { SnowflakeComponent } from './app/snowflake/snowflake.component';
+import { VehicleComponent } from './app/vehicle/vehicle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameCanvasComponent,
+    SnowflakeComponent,
+    VehicleComponent
   ],
   imports: [
     BrowserModule
