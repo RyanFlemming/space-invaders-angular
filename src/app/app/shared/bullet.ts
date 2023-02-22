800841882a7088ceeba1408ddd3ee23a5bbc590b
Ryan Flemming <rmf919@live.com>
2023-02-22 15:27:33 +1100

    add game object

--- src/app/app/shared/bullet.ts
+++ src/app/app/shared/bullet.ts
@@ -1,2 +1,11 @@
-export class Bullet {
+import { GameObject } from "./game-object";
+
+export class Bullet extends GameObject {
+
+    super(x: number, y: number, width: number, height: number) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+      }
 }
---
+++ src/app/app/shared/game-object.ts
@@ -0,0 +1,7 @@
+export class GameObject {
+    x!: number;
+    y!: number;
+    width!: number;
+    height!: number;
+    // to do: add color
+}
