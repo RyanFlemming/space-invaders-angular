a18453ca300d3440c66d993c169563a75ae631c2
Ryan Flemming <rmf919@live.com>
2023-03-20 15:30:24 +1100

    add behavior for bullets

--- src/app/app/game-canvas/game-canvas.component.ts
+++ src/app/app/game-canvas/game-canvas.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Bullet } from '../shared/bullet';
 import { GameObject } from '../shared/game-object';
 
 @Component({
@@ -8,10 +9,13 @@ import { GameObject } from '../shared/game-object';
 })
 export class GameCanvasComponent implements OnInit {
   gameObj: GameObject;
+  bulletObj: Bullet;
   @ViewChild('canvas', {static: true}) myCanvas!: ElementRef<HTMLCanvasElement>;
 
   constructor(){
-    this.gameObj = new GameObject(5, 5, 100, 100)
+    // add obj's to canvas, replace later
+    this.gameObj = new GameObject(5, 5, 100, 100);
+    this.bulletObj = new Bullet(200, 500, 5, 5, 1);
   }
 
   ngOnInit(): void {
@@ -21,6 +25,8 @@ export class GameCanvasComponent implements OnInit {
     if(context) {
       this.defineCanvas(context);
       this.gameObj.draw(context);
+      this.bulletObj.draw(context);
+      
     }
 
   }
